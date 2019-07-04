8350de453aa68071b98d9cc87df7a21e4edca547
arcanrun <arcan.run@gmail.com>
2019-07-04 03:05:07 +0300

    Updates

--- precache-manifest.ca2187615addec74103e7d243af56338.js
+++ precache-manifest.ca2187615addec74103e7d243af56338.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/budget-manager/static/media/calendar-number.763d99ab.svg"
   },
   {
-    "revision": "2696d8289132c08434e7",
+    "revision": "45715f78f055e0b0db24",
     "url": "/budget-manager/static/css/main.7b51393f.chunk.css"
   },
   {
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "url": "/budget-manager/static/media/calendar.c4148bfc.svg"
   },
   {
-    "revision": "2696d8289132c08434e7",
-    "url": "/budget-manager/static/js/main.240e460e.chunk.js"
+    "revision": "45715f78f055e0b0db24",
+    "url": "/budget-manager/static/js/main.f0e2d66b.chunk.js"
   },
   {
     "revision": "2e7c669177c13a55f839f3e624f91174",
@@ -116,7 +116,7 @@ self.__precacheManifest = [
     "url": "/budget-manager/static/css/2.55b6d123.chunk.css"
   },
   {
-    "revision": "0cbf5d26ab9639b7f2747ff9ba4cd34f",
+    "revision": "32fbc5f294b685d9b546c5ce4371b025",
     "url": "/budget-manager/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/budget-manager/precache-manifest.707c4eebe4daa020e3bc993d0c240a28.js"
+  "/budget-manager/precache-manifest.ca2187615addec74103e7d243af56338.js"
 );
 
 workbox.clientsClaim();
