07fb9f6cafb2e67d455665f2408ed09a2a769d4e
arcanrun <arcan.run@gmail.com>
2019-08-30 17:09:37 +0300

    Updates

--- precache-manifest.d1eb78cdf8679c4f7e1a609d80be5e2d.js
+++ precache-manifest.d1eb78cdf8679c4f7e1a609d80be5e2d.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/budget-manager/static/media/fa-solid-900.6ed5e3bc.svg"
   },
   {
-    "revision": "aa03739e1d8373a6b6b8",
+    "revision": "6bbc4478c6de488bf761",
     "url": "/budget-manager/static/css/main.7c94434e.chunk.css"
   },
   {
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/budget-manager/static/media/cross-white.9ca6ab3b.svg"
   },
   {
-    "revision": "38abd07a31646eacd551f3715421da7f",
-    "url": "/budget-manager/static/media/plus.38abd07a.svg"
-  },
-  {
     "revision": "0ebebd45a566793c2444d01bcfcff373",
     "url": "/budget-manager/static/media/minus.0ebebd45.svg"
+  },
+  {
+    "revision": "38abd07a31646eacd551f3715421da7f",
+    "url": "/budget-manager/static/media/plus.38abd07a.svg"
   },
   {
     "revision": "8dab467d1c9d8665eab577e474c72de2",
@@ -84,20 +84,20 @@ self.__precacheManifest = [
     "url": "/budget-manager/static/media/fa-regular-400.95f13e0b.svg"
   },
   {
-    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
-    "url": "/budget-manager/static/media/fa-solid-900.42e1fbd2.eot"
-  },
-  {
     "revision": "896e20e26ad07dc63c9526ed814117e9",
     "url": "/budget-manager/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/budget-manager/static/media/fa-solid-900.42e1fbd2.eot"
   },
   {
     "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
     "url": "/budget-manager/static/media/fa-brands-400.bfa9c38b.svg"
   },
   {
-    "revision": "aa03739e1d8373a6b6b8",
-    "url": "/budget-manager/static/js/main.5d97757b.chunk.js"
+    "revision": "6bbc4478c6de488bf761",
+    "url": "/budget-manager/static/js/main.c713a770.chunk.js"
   },
   {
     "revision": "773dcb53a49a6ea9ac49194e947207d7",
@@ -192,7 +192,7 @@ self.__precacheManifest = [
     "url": "/budget-manager/static/css/2.f333a2c4.chunk.css"
   },
   {
-    "revision": "b3c0c9b427ae7110ee3e77e1fbdc1ab6",
+    "revision": "a5d0338762f7702f34185ecbf2b0a188",
     "url": "/budget-manager/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/budget-manager/precache-manifest.7ecb589f4b94c7cb2ef2be50f96196e1.js"
+  "/budget-manager/precache-manifest.d1eb78cdf8679c4f7e1a609d80be5e2d.js"
 );
 
 workbox.clientsClaim();
